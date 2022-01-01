119978d696bd929f4097efc7fd940450889a9b8d
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-01 13:49:13 +0600

    Add new expense form
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
+import { v4 as uuidv4 } from "uuid";
 
 const AddExpenseForm = () => {
+  const { dispatch } = useContext(AppContext);
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const expense = {
+      id: uuidv4(),
+      name: name,
+      cost: parseInt(cost),
+    };
+
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="row">
         <div className="col-sm">
           <label for="name">Name</label>
@@ -11,6 +32,8 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </div>
         <div className="col-sm">
@@ -20,6 +43,8 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="cost"
+            value={cost}
+            onChange={(event) => setCost(event.target.value)}
           />
         </div>
         <div className="col-sm">
